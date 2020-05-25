58d686e3587db2b53b639787eef9ed3f63cd2145
codegitpro <yusufqing920@gmail.com>
2020-05-24 21:34:33 -0700

    feat: 🎸 update project strture

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 
 gulp.task('sass', function () {
-  return gulp.src('./app/styles/index.scss')
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(gulp.dest('./app/dist'))
+    return gulp
+        .src('./app/styles/index.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./app/dist/'))
 })
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./app/**/*.scss', ['sass'])
+    gulp.watch('./app/**/*.scss', ['sass'])
 })
 
 gulp.task('default', ['sass'])
